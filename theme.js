471caa5023b2157321337c172909faf30deeeedd
Amp3rs4n <kisjlyk97@gmail.com>
2025-06-01 19:54:28 +0300

    update

--- theme.js
+++ theme.js
@@ -1,23 +1,19 @@
 // theme.js
 
-// Function to apply the saved theme
 function applySavedTheme() {
   const savedTheme = localStorage.getItem('theme');
   const body = document.body;
   const metaThemeColor = document.querySelector("meta[name=theme-color]");
 
-  if (savedTheme === 'light-mode') {
-    body.classList.add('light-mode');
-    updateThemeButtonText('light-mode');
-    if (metaThemeColor) metaThemeColor.setAttribute('content', '#f4f4f4');
-  } else {
-    body.classList.remove('light-mode');
-    updateThemeButtonText('dark-mode');
-    if (metaThemeColor) metaThemeColor.setAttribute('content', '#0f0f1b');
+  const isLight = savedTheme === 'light-mode';
+  body.classList.toggle('light-mode', isLight);
+  updateThemeButtonText(isLight ? 'light-mode' : 'dark-mode');
+
+  if (metaThemeColor) {
+    metaThemeColor.setAttribute('content', isLight ? '#f4f4f4' : '#0f0f1b');
   }
 }
 
-// Function to update the theme toggle button text
 function updateThemeButtonText(theme) {
   const themeToggleBtn = document.getElementById('themeToggleBtn');
   if (themeToggleBtn) {
@@ -25,7 +21,6 @@ function updateThemeButtonText(theme) {
   }
 }
 
-// Function to initialize the theme toggle button
 function initializeThemeToggle() {
   const themeToggleBtn = document.getElementById('themeToggleBtn');
   const body = document.body;
@@ -33,19 +28,18 @@ function initializeThemeToggle() {
 
   if (themeToggleBtn) {
     themeToggleBtn.addEventListener('click', () => {
-      body.classList.toggle('light-mode');
+      const isNowLight = !body.classList.contains('light-mode');
+      body.classList.toggle('light-mode', isNowLight);
+      localStorage.setItem('theme', isNowLight ? 'light-mode' : '');
+      updateThemeButtonText(isNowLight ? 'light-mode' : 'dark-mode');
 
-      const isLight = body.classList.contains('light-mode');
-      localStorage.setItem('theme', isLight ? 'light-mode' : '');
-      updateThemeButtonText(isLight ? 'light-mode' : 'dark-mode');
-      if (metaThemeColor)
-        metaThemeColor.setAttribute('content', isLight ? '#f4f4f4' : '#0f0f1b');
+      if (metaThemeColor) {
+        metaThemeColor.setAttribute('content', isNowLight ? '#f4f4f4' : '#0f0f1b');
+      }
     });
   }
 }
 
-// Apply the saved theme when the page loads
+// Init
 applySavedTheme();
-
-// Initialize the theme toggle button
 initializeThemeToggle();
