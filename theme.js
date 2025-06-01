7b40cbceaddcfda34425e55c21d50fda000a497b
Amp3rs4n <kisjlyk97@gmail.com>
2025-06-01 19:45:17 +0300

    update

--- theme.js
+++ theme.js
@@ -4,23 +4,24 @@
 function applySavedTheme() {
   const savedTheme = localStorage.getItem('theme');
   const body = document.body;
+  const metaThemeColor = document.querySelector("meta[name=theme-color]");
 
   if (savedTheme === 'light-mode') {
     body.classList.add('light-mode');
     updateThemeButtonText('light-mode');
+    if (metaThemeColor) metaThemeColor.setAttribute('content', '#f4f4f4');
   } else {
     body.classList.remove('light-mode');
     updateThemeButtonText('dark-mode');
+    if (metaThemeColor) metaThemeColor.setAttribute('content', '#0f0f1b');
   }
 }
 
 // Function to update the theme toggle button text
 function updateThemeButtonText(theme) {
   const themeToggleBtn = document.getElementById('themeToggleBtn');
-  if (theme === 'light-mode') {
-    themeToggleBtn.textContent = '🌞 Light Mode';
-  } else {
-    themeToggleBtn.textContent = '🌙 Dark Mode';
+  if (themeToggleBtn) {
+    themeToggleBtn.textContent = theme === 'light-mode' ? '🌞 Light Mode' : '🌙 Dark Mode';
   }
 }
 
@@ -28,17 +29,17 @@ function updateThemeButtonText(theme) {
 function initializeThemeToggle() {
   const themeToggleBtn = document.getElementById('themeToggleBtn');
   const body = document.body;
+  const metaThemeColor = document.querySelector("meta[name=theme-color]");
 
   if (themeToggleBtn) {
     themeToggleBtn.addEventListener('click', () => {
       body.classList.toggle('light-mode');
-      // Save the current theme in localStorage
-      if (body.classList.contains('light-mode')) {
-        localStorage.setItem('theme', 'light-mode');
-      } else {
-        localStorage.removeItem('theme');
-      }
-      updateThemeButtonText(body.classList.contains('light-mode') ? 'light-mode' : 'dark-mode');
+
+      const isLight = body.classList.contains('light-mode');
+      localStorage.setItem('theme', isLight ? 'light-mode' : '');
+      updateThemeButtonText(isLight ? 'light-mode' : 'dark-mode');
+      if (metaThemeColor)
+        metaThemeColor.setAttribute('content', isLight ? '#f4f4f4' : '#0f0f1b');
     });
   }
 }
